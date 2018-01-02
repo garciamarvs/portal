66273498a713eb12485a03d49b4fd55bc5be0e02
gummya <silentguyxD@gmail.com>
2018-01-03 03:46:19 +0800

    .

--- portal.sql
+++ portal.sql
@@ -1,32 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 29, 2017 at 05:14 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.11
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `portal`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `course`
---
 
 CREATE TABLE `course` (
   `id` int(11) NOT NULL,
@@ -40,21 +21,11 @@ CREATE TABLE `course` (
   `sem_id` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `course`
---
-
 INSERT INTO `course` (`id`, `title`, `code`, `unit`, `schedule`, `room`, `instructor`, `section_id`, `sem_id`) VALUES
 (1, 'Data Communication & Basic Network Concepts', 'ACS311', '3', '', '', '2', '', '1'),
 (2, 'Database Management Systems 2', 'ACS312', '3', '', '', '3', '', '1'),
 (3, 'Multimedia Technology I', 'ACS313', '3', '', '', '2', '', '1'),
 (4, 'Object Oriented Programming', 'ACS314', '3', '', '', '3', '', '1');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `course_log`
---
 
 CREATE TABLE `course_log` (
   `id` int(11) NOT NULL,
@@ -68,10 +39,6 @@ CREATE TABLE `course_log` (
   `remarks` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `course_log`
---
-
 INSERT INTO `course_log` (`id`, `sem_id`, `course`, `student`, `grade`, `npe`, `ope`, `le`, `remarks`) VALUES
 (1, '1', '1', '1', '', '', '', '', ''),
 (2, '1', '2', '1', '', '', '', '', ''),
@@ -82,12 +49,6 @@ INSERT INTO `course_log` (`id`, `sem_id`, `course`, `student`, `grade`, `npe`, `
 (7, '1', '3', '4', '', '', '', '', ''),
 (8, '1', '4', '4', '', '', '', '', '');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `eval_log`
---
-
 CREATE TABLE `eval_log` (
   `id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL,
@@ -96,23 +57,11 @@ CREATE TABLE `eval_log` (
   `comment` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `eval_log`
---
-
 INSERT INTO `eval_log` (`id`, `student_id`, `course_id`, `result`, `comment`) VALUES
-(4, 1, 1, '[{\"ID\":\"A1\",\"Value\":\"5\"},{\"ID\":\"A2\",\"Value\":\"5\"},{\"ID\":\"B3\",\"Value\":\"5\"},{\"ID\":\"B4\",\"Value\":\"5\"},{\"ID\":\"C5\",\"Value\":\"5\"},{\"ID\":\"C6\",\"Value\":\"5\"},{\"ID\":\"D7\",\"Value\":\"5\"},{\"ID\":\"D8\",\"Value\":\"5\"},{\"ID\":\"E9\",\"Value\":\"5\"},{\"ID\":\"E10\",\"Value\":\"5\"}]', '\'\''),
-(7, 1, 2, '[{\"ID\":\"A1\",\"Value\":\"5\"},{\"ID\":\"A2\",\"Value\":\"5\"},{\"ID\":\"B3\",\"Value\":\"5\"},{\"ID\":\"B4\",\"Value\":\"5\"},{\"ID\":\"C5\",\"Value\":\"5\"},{\"ID\":\"C6\",\"Value\":\"5\"},{\"ID\":\"D7\",\"Value\":\"5\"},{\"ID\":\"D8\",\"Value\":\"5\"},{\"ID\":\"E9\",\"Value\":\"5\"},{\"ID\":\"E10\",\"Value\":\"5\"}]', '\'\''),
-(8, 1, 3, '[{\"ID\":\"A1\",\"Value\":\"5\"},{\"ID\":\"A2\",\"Value\":\"5\"},{\"ID\":\"B3\",\"Value\":\"5\"},{\"ID\":\"B4\",\"Value\":\"5\"},{\"ID\":\"C5\",\"Value\":\"5\"},{\"ID\":\"C6\",\"Value\":\"5\"},{\"ID\":\"D7\",\"Value\":\"5\"},{\"ID\":\"D8\",\"Value\":\"5\"},{\"ID\":\"E9\",\"Value\":\"5\"},{\"ID\":\"E10\",\"Value\":\"5\"}]', '\'\''),
-(9, 1, 3, '[{\"ID\":\"A1\",\"Value\":\"5\"},{\"ID\":\"A2\",\"Value\":\"5\"},{\"ID\":\"B3\",\"Value\":\"5\"},{\"ID\":\"B4\",\"Value\":\"5\"},{\"ID\":\"C5\",\"Value\":\"5\"},{\"ID\":\"C6\",\"Value\":\"5\"},{\"ID\":\"D7\",\"Value\":\"5\"},{\"ID\":\"D8\",\"Value\":\"5\"},{\"ID\":\"E9\",\"Value\":\"5\"},{\"ID\":\"E10\",\"Value\":\"5\"}]', '\'\''),
-(10, 1, 4, '[{\"ID\":\"A1\",\"Value\":\"5\"},{\"ID\":\"A2\",\"Value\":\"5\"},{\"ID\":\"B3\",\"Value\":\"5\"},{\"ID\":\"B4\",\"Value\":\"5\"},{\"ID\":\"C5\",\"Value\":\"5\"},{\"ID\":\"C6\",\"Value\":\"5\"},{\"ID\":\"D7\",\"Value\":\"5\"},{\"ID\":\"D8\",\"Value\":\"5\"},{\"ID\":\"E9\",\"Value\":\"5\"},{\"ID\":\"E10\",\"Value\":\"5\"}]', '\'\''),
-(11, 4, 3, '[{\"ID\":\"A1\",\"Value\":\"4\"},{\"ID\":\"A2\",\"Value\":\"5\"},{\"ID\":\"B3\",\"Value\":\"5\"},{\"ID\":\"B4\",\"Value\":\"5\"},{\"ID\":\"C5\",\"Value\":\"5\"},{\"ID\":\"C6\",\"Value\":\"5\"},{\"ID\":\"D7\",\"Value\":\"5\"},{\"ID\":\"D8\",\"Value\":\"5\"},{\"ID\":\"E9\",\"Value\":\"5\"},{\"ID\":\"E10\",\"Value\":\"5\"}]', '\'\'');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `questions`
---
+(18, 1, 1, '[{\"ID\":\"A1\",\"Value\":\"4\"},{\"ID\":\"A2\",\"Value\":\"4\"},{\"ID\":\"B3\",\"Value\":\"4\"},{\"ID\":\"B4\",\"Value\":\"4\"},{\"ID\":\"C5\",\"Value\":\"4\"},{\"ID\":\"C6\",\"Value\":\"4\"},{\"ID\":\"D7\",\"Value\":\"4\"},{\"ID\":\"D8\",\"Value\":\"4\"},{\"ID\":\"E9\",\"Value\":\"4\"},{\"ID\":\"E10\",\"Value\":\"4\"}]', '\'\''),
+(19, 4, 1, '[{\"ID\":\"A1\",\"Value\":\"3\"},{\"ID\":\"A2\",\"Value\":\"3\"},{\"ID\":\"B3\",\"Value\":\"3\"},{\"ID\":\"B4\",\"Value\":\"3\"},{\"ID\":\"C5\",\"Value\":\"3\"},{\"ID\":\"C6\",\"Value\":\"3\"},{\"ID\":\"D7\",\"Value\":\"3\"},{\"ID\":\"D8\",\"Value\":\"3\"},{\"ID\":\"E9\",\"Value\":\"3\"},{\"ID\":\"E10\",\"Value\":\"3\"}]', '\'\''),
+(20, 1, 3, '[{\"ID\":\"A1\",\"Value\":\"4\"},{\"ID\":\"A2\",\"Value\":\"4\"},{\"ID\":\"B3\",\"Value\":\"5\"},{\"ID\":\"B4\",\"Value\":\"5\"},{\"ID\":\"C5\",\"Value\":\"5\"},{\"ID\":\"C6\",\"Value\":\"3\"},{\"ID\":\"D7\",\"Value\":\"4\"},{\"ID\":\"D8\",\"Value\":\"3\"},{\"ID\":\"E9\",\"Value\":\"5\"},{\"ID\":\"E10\",\"Value\":\"4\"}]', '\'\''),
+(22, 4, 3, '[{\"ID\":\"A1\",\"Value\":\"3\"},{\"ID\":\"A2\",\"Value\":\"5\"},{\"ID\":\"B3\",\"Value\":\"4\"},{\"ID\":\"B4\",\"Value\":\"4\"},{\"ID\":\"C5\",\"Value\":\"3\"},{\"ID\":\"C6\",\"Value\":\"5\"},{\"ID\":\"D7\",\"Value\":\"5\"},{\"ID\":\"D8\",\"Value\":\"5\"},{\"ID\":\"E9\",\"Value\":\"4\"},{\"ID\":\"E10\",\"Value\":\"5\"}]', '\'\'');
 
 CREATE TABLE `questions` (
   `id` int(11) NOT NULL,
@@ -121,10 +70,6 @@ CREATE TABLE `questions` (
   `description` text NOT NULL,
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `questions`
---
 
 INSERT INTO `questions` (`id`, `category`, `question`, `description`, `active`) VALUES
 (1, 'A', 'The instructor clearly presented the skills to be learned', '', 1),
@@ -138,20 +83,10 @@ INSERT INTO `questions` (`id`, `category`, `question`, `description`, `active`) 
 (9, 'E', 'The instructor engaged the class in productive discussions', '', 1),
 (10, 'E', 'The instructor guided the discussion well', '', 1);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `ref_college`
---
-
 CREATE TABLE `ref_college` (
   `name` varchar(255) NOT NULL,
   `code` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `ref_college`
---
 
 INSERT INTO `ref_college` (`name`, `code`) VALUES
 ('College of Accountancy and Economics', 'CAE'),
@@ -166,32 +101,17 @@ INSERT INTO `ref_college` (`name`, `code`) VALUES
 ('College of Mass Communication', 'CMC'),
 ('College of Public Administration', 'CPA');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `semester`
---
-
 CREATE TABLE `semester` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `semester`
---
-
 INSERT INTO `semester` (`id`, `name`) VALUES
 (1, 'SY2017-2018 First Semester'),
 (2, 'SY2017-2018 Second Semester');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `status`
---
-
 CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `sem_id` varchar(255) NOT NULL,
   `active` tinyint(1) NOT NULL,
@@ -199,18 +119,10 @@ CREATE TABLE `status` (
   `date` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `status`
---
-
-INSERT INTO `status` (`name`, `sem_id`, `active`, `start_date`, `date`) VALUES
-('faculty_eval', '1', 1, '', '30-12-2017 24:00:00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
+INSERT INTO `status` (`id`, `name`, `sem_id`, `active`, `start_date`, `date`) VALUES
+(1, 'faculty_eval', '1', 0, '', '12/30/2017 24:00:00'),
+(3, 'faculty_eval', '1', 0, '12/31/2017 00:00:00', '01/01/2018 24:00:00'),
+(4, 'faculty_eval', '1', 1, '01/01/2018 00:00:00', '01/08/2018 24:00:00');
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
@@ -223,10 +135,6 @@ CREATE TABLE `user` (
   `college` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `user`
---
-
 INSERT INTO `user` (`id`, `username`, `password`, `first_name`, `middle_name`, `last_name`, `usertype`, `college`) VALUES
 (1, '1student', 'password', 'Monkey', 'D.', 'Luffy', '1', 'CET'),
 (2, '1faculty', 'password', 'Silvers', '', 'Rayleigh', '2', 'CAE'),
@@ -234,95 +142,50 @@ INSERT INTO `user` (`id`, `username`, `password`, `first_name`, `middle_name`, `
 (4, '2student', 'password', 'Vinsmoke', '', 'Sanji', '1', 'CET'),
 (5, 'admin', '12345', 'Eiichiro', '', 'Oda', '5', '');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `course`
---
 ALTER TABLE `course`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `course_log`
---
 ALTER TABLE `course_log`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `eval_log`
---
 ALTER TABLE `eval_log`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `questions`
---
 ALTER TABLE `questions`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `ref_college`
---
 ALTER TABLE `ref_college`
   ADD UNIQUE KEY `code` (`code`);
 
---
--- Indexes for table `semester`
---
 ALTER TABLE `semester`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `status`
---
 ALTER TABLE `status`
-  ADD UNIQUE KEY `name` (`name`);
+  ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `course`
---
 ALTER TABLE `course`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `course_log`
---
 ALTER TABLE `course_log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
---
--- AUTO_INCREMENT for table `eval_log`
---
 ALTER TABLE `eval_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
---
--- AUTO_INCREMENT for table `questions`
---
 ALTER TABLE `questions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
---
--- AUTO_INCREMENT for table `semester`
---
 ALTER TABLE `semester`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `user`
---
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
