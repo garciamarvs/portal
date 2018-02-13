25e4ad3fe0e1f4b805fa4cb23675348aa3d57699
gummya <silentguyxD@gmail.com>
2018-02-14 00:05:22 +0800

    2 14

--- application/views/home.php
+++ application/views/home.php
@@ -6,3 +6,10 @@
 // }
 
 ?>
+
+<div class="row m-t-md">
+	<div style="background: url('<?= base_url()?>assets/img/header_two.png') no-repeat;background-position:center;background-size: 90% 90%; min-height: 500px;">
+	</div>
+	<div style="position: absolute;top: 0;bottom: 0;background-color: rgba(0,0,0,0.6);height: 100%;width: 100%;"></div>		
+	<h1 style="position: absolute;top: 50%;left: 50%; -webkit-transform: translate(-50%,-50%); color: white; font-weight: bold; font-size: 50px;">Welcome to <span style="color:#4dff4d;">UDM</span> Portal</h1>
+</div>
--- application/views/login.php
+++ application/views/login.php
@@ -41,8 +41,8 @@
 					<!-- <p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.</p>
 
 					<p>
-						<small>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</small> -->
-					</p>
+						<small>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</small>
+					</p> -->
 				</div>
 				<div class="col-md-6">
 					<div class="ibox-content">
